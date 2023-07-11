aa6678e01765eb2472f73ecaf999bf6ddf132659
GitEagleY <ch0tati@proton.me>
2023-07-11 19:24:16 +0300

    refactored render

--- pkg/Render/render.go
+++ pkg/Render/render.go
@@ -1,39 +1,62 @@
 package render
 
 import (
-	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
-var templateCache = make(map[string]*template.Template) //template cache
-
-func RenderTemplate(w http.ResponseWriter, templateName string) {
-	var templateToRender *template.Template
-	_, isInMap := templateCache[templateName]
-	if !isInMap {
-		err := cacheTemplate(templateName)
-		if err != nil {
-			return
-		}
-		fmt.Println("cached")
-	} else {
-		fmt.Println("using cached")
-	}
-	templateToRender = templateCache[templateName]
-	templateToRender.Execute(w, nil)
-}
-
-func cacheTemplate(templateName string) error {
-	tmplts := []string{
-		fmt.Sprintf("./templates/%s", templateName),
-		"./templates/base.layout.tmpl",
-	}
-
-	templateParsed, err := template.ParseFiles(tmplts...)
+func RenderTemplate(w http.ResponseWriter, templateName string) error {
+	var err error
+	templateCache, err := cacheTemplate()
 	if err != nil {
+		log.Fatal(err)
 		return err
 	}
-	templateCache[templateName] = templateParsed
+	//make template to render
+	templateToRender, ok := templateCache[templateName]
+
+	if !ok {
+		log.Fatal(ok)
+
+	}
+	//render template
+	templateToRender = templateCache[templateName]
+	templateToRender.Execute(w, nil)
 	return nil
+}
+func cacheTemplate() (map[string]*template.Template, error) {
+	Cache := map[string]*template.Template{}
+
+	// get all of the files named *.page.tmpl from ./templates
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return Cache, err
+	}
+
+	// range through all files ending with *.page.tmpl
+	for _, page := range pages {
+		name := filepath.Base(page)
+		tmplts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return Cache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return Cache, err
+		}
+
+		if len(matches) > 0 {
+			tmplts, err = tmplts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return Cache, err
+			}
+		}
+
+		Cache[name] = tmplts
+	}
+
+	return Cache, nil
 }
